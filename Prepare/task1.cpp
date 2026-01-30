5cb91ec9e4f08835e9ee52069be370b062f227b2
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-01-30 15:53:10 +0200

    add

--- Prepare/task1.cpp
+++ Prepare/task1.cpp
@@ -13,30 +13,39 @@
 #include <cstdint>
 #include <cstdarg>
 
-typedef long long ll;
+typedef long long ll ;
+typedef long double ld;
 using namespace std;
-
-void summary(ll n, ll k, ll leftcordon, vector<ll>& a){
+void summary(ll n,ll k,ll leftcordon,vector<ll>& a){
     if(k==0){
         for(ll i:a)cout<<i<<" ";
         cout<<"\n";
         return;
     }
-    ll start = (leftcordon == 0) ? 1 : leftcordon + 1;
-    for(ll i=start; i<=n;++i){
+    for( ll i=leftcordon+1;i<=n;++i){
         if(leftcordon==0 || i%leftcordon==0){
             a.push_back(i);
-            summary(n,k-1,i,a);
+            summary(n, k - 1, i, a);
             a.pop_back();
         }
     }
 }
+
+ll factorial( ll n){
+    if(n==0)return 1;
+    return n* factorial(n-1); 
+}
+
 int main(){
     ll n=0,k=0;
     cin>>n;
     cin>>k;
-    vector<ll> fun;
-    summary(n,k,0,fun);
+    vector<ll> a;
+    summary(n,k,0,a);
+    cout<<"done";
+    cout<<"\n";
+    cin>>n;
+    cout<<factorial(n)<<endl;
     cout<<"done";
     return 0;
 }
