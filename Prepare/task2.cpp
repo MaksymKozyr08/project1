eb2aea6560c61956a538e715b1b4dcf5430cce4b
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-02-03 17:41:49 +0200

    test

---
+++ Prepare/task2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ratio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cstdarg>
+
+
+typedef long long ll ;
+typedef long double ld;
+using namespace std;
+
+
+string prefix(vector<string>& a){
+    for(ll i=0;i<a.size();++i){
+    }
+}
+int main(){
+    string s;
+    return 0;
+}
