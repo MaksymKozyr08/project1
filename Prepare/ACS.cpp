7707d5c6b4f4c2b2789982b46c8a662715051032
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-02-09 23:46:40 +0200

    finish task

--- Prepare/ACS.cpp
+++ Prepare/ACS.cpp
@@ -6,159 +6,156 @@
 
 using namespace std;
 
-// Налаштування: 50 млн ітерацій для старту
-const long BASE_N = 50000000; 
-const int K_RUNS = 5;
+const long N_BASE = 50000000; 
+const int RUNS = 5;
 
-struct Result { string name; double ops; };
+struct Res {
+    string name;
+    double val;
+};
 
-// --- ФУНКЦІЯ ДЛЯ INT ---
 double test_int(char op) {
-    long N = BASE_N;
-    if (op == '/') N /= 5; // Ділення повільне, зменшуємо ітерації
+    long n = N_BASE;
+    if (op == '/') n /= 5; 
 
-    double total_ops = 0;
-    // volatile int - щоб компілятор не викинув змінні
+    double sum_ops = 0;
     volatile int a = 10, b = 20, c = 0;
+    
     if (op == '/' || op == '*') { a=10000; b=2; c=1; }
 
-    for (int k = 0; k < K_RUNS; k++) {
-        // 1. Час роботи (Математика + Цикл)
+    for (int k = 0; k < RUNS; k++) {
         clock_t t1 = clock();
-        for (long i = 0; i < N; i++) {
-            if (op == '+')      { c=a+b; b=a+c; a=b+c; }
-            else if (op == '-') { c=a-b; b=a-c; a=b-c; }
-            else if (op == '*') { c=a*b; b=a*c; a=b*c; if(i%100==0){a=2;b=3;} }
-            else if (op == '/') { c=a/b; b=c+1; a=b+10; if(b==0)b=1; }
+        for (long i = 0; i < n; i++) {
+            if (op == '+') { c = a + b; b = a + c; a = b + c; }
+            else if (op == '-') { c = a - b; b = a - c; a = b - c; }
+            else if (op == '*') { c = a * b; b = a * c; a = b * c; if (i % 100 == 0) { a=2; b=3; } }
+            else if (op == '/') { c = a / b; b = c + 1; a = b + 10; if (b == 0) b = 1; }
         }
         clock_t t2 = clock();
 
-        // Збереження
-        int ta=a, tb=b, tc=c;
+        int ta = a, tb = b, tc = c;
 
-        // 2. Час порожнього циклу
-        clock_t t01 = clock();
-        for (long i = 0; i < N; i++) {
-            c=a; b=c; a=b;
+        clock_t t3 = clock();
+        for (long i = 0; i < n; i++) {
+            c = a; b = c; a = b;
         }
-        clock_t t02 = clock();
+        clock_t t4 = clock();
 
-        double t = double((t2 - t1) - (t02 - t01)) / CLOCKS_PER_SEC;
-        if (t < 1e-9) t = 1e-9;
+        double time = double((t2 - t1) - (t4 - t3)) / CLOCKS_PER_SEC;
+        if (time < 1e-9) time = 1e-9;
         
-        // 3 операції * N ітерацій
-        total_ops += (3.0 * N) / t;
+        sum_ops += (3.0 * n) / time;
     }
-    return total_ops / K_RUNS;
+    return sum_ops / RUNS;
 }
 
-// --- ФУНКЦІЯ ДЛЯ LONG ---
 double test_long(char op) {
-    long N = BASE_N;
-    if (op == '/') N /= 5;
+    long n = N_BASE;
+    if (op == '/') n /= 5;
 
-    double total_ops = 0;
+    double sum_ops = 0;
     volatile long a = 10, b = 20, c = 0;
     if (op == '/' || op == '*') { a=10000; b=2; c=1; }
 
-    for (int k = 0; k < K_RUNS; k++) {
+    for (int k = 0; k < RUNS; k++) {
         clock_t t1 = clock();
-        for (long i = 0; i < N; i++) {
-            if (op == '+')      { c=a+b; b=a+c; a=b+c; }
-            else if (op == '-') { c=a-b; b=a-c; a=b-c; }
-            else if (op == '*') { c=a*b; b=a*c; a=b*c; if(i%100==0){a=2;b=3;} }
-            else if (op == '/') { c=a/b; b=c+1; a=b+10; if(b==0)b=1; }
+        for (long i = 0; i < n; i++) {
+            if (op == '+') { c = a + b; b = a + c; a = b + c; }
+            else if (op == '-') { c = a - b; b = a - c; a = b - c; }
+            else if (op == '*') { c = a * b; b = a * c; a = b * c; if (i % 100 == 0) { a=2; b=3; } }
+            else if (op == '/') { c = a / b; b = c + 1; a = b + 10; if (b == 0) b = 1; }
         }
         clock_t t2 = clock();
 
-        long ta=a, tb=b, tc=c;
+        long ta = a, tb = b, tc = c;
 
-        clock_t t01 = clock();
-        for (long i = 0; i < N; i++) {
-            c=a; b=c; a=b;
+        clock_t t3 = clock();
+        for (long i = 0; i < n; i++) {
+            c = a; b = c; a = b;
         }
-        clock_t t02 = clock();
+        clock_t t4 = clock();
 
-        double t = double((t2 - t1) - (t02 - t01)) / CLOCKS_PER_SEC;
-        if (t < 1e-9) t = 1e-9;
-        total_ops += (3.0 * N) / t;
+        double time = double((t2 - t1) - (t4 - t3)) / CLOCKS_PER_SEC;
+        if (time < 1e-9) time = 1e-9;
+        sum_ops += (3.0 * n) / time;
     }
-    return total_ops / K_RUNS;
+    return sum_ops / RUNS;
 }
 
-// --- ФУНКЦІЯ ДЛЯ DOUBLE ---
 double test_double(char op) {
-    long N = BASE_N;
-    if (op == '/') N /= 5; 
+    long n = N_BASE;
+    if (op == '/') n /= 5;
 
-    double total_ops = 0;
+    double sum_ops = 0;
     volatile double a = 10, b = 20, c = 0;
     if (op == '/' || op == '*') { a=10000; b=2; c=1; }
 
-    for (int k = 0; k < K_RUNS; k++) {
+    for (int k = 0; k < RUNS; k++) {
         clock_t t1 = clock();
-        for (long i = 0; i < N; i++) {
-            if (op == '+')      { c=a+b; b=a+c; a=b+c; }
-            else if (op == '-') { c=a-b; b=a-c; a=b-c; }
-            else if (op == '*') { c=a*b; b=a*c; a=b*c; if(i%100==0){a=2;b=3;} }
-            else if (op == '/') { c=a/b; b=c+1; a=b+10; if(b==0)b=1; }
+        for (long i = 0; i < n; i++) {
+            if (op == '+') { c = a + b; b = a + c; a = b + c; }
+            else if (op == '-') { c = a - b; b = a - c; a = b - c; }
+            else if (op == '*') { c = a * b; b = a * c; a = b * c; if (i % 100 == 0) { a=2; b=3; } }
+            else if (op == '/') { c = a / b; b = c + 1; a = b + 10; if (b == 0) b = 1; }
         }
         clock_t t2 = clock();
 
-        double ta=a, tb=b, tc=c;
+        double ta = a, tb = b, tc = c;
 
-        clock_t t01 = clock();
-        for (long i = 0; i < N; i++) {
-            c=a; b=c; a=b;
+        clock_t t3 = clock();
+        for (long i = 0; i < n; i++) {
+            c = a; b = c; a = b;
         }
-        clock_t t02 = clock();
+        clock_t t4 = clock();
 
-        double t = double((t2 - t1) - (t02 - t01)) / CLOCKS_PER_SEC;
-        if (t < 1e-9) t = 1e-9;
-        total_ops += (3.0 * N) / t;
+        double time = double((t2 - t1) - (t4 - t3)) / CLOCKS_PER_SEC;
+        if (time < 1e-9) time = 1e-9;
+        sum_ops += (3.0 * n) / time;
     }
-    return total_ops / K_RUNS;
+    return sum_ops / RUNS;
 }
 
 int main() {
-    vector<Result> results;
+    vector<Res> v;
 
-    // Ручне додавання тестів
-    results.push_back({"int +", test_int('+')});
-    results.push_back({"int -", test_int('-')});
-    results.push_back({"int *", test_int('*')});
-    results.push_back({"int /", test_int('/')});
+    v.push_back({"int +", test_int('+')});
+    v.push_back({"int -", test_int('-')});
+    v.push_back({"int *", test_int('*')});
+    v.push_back({"int /", test_int('/')});
 
-    results.push_back({"long +", test_long('+')});
-    results.push_back({"long -", test_long('-')});
-    results.push_back({"long *", test_long('*')});
-    results.push_back({"long /", test_long('/')});
+    v.push_back({"long +", test_long('+')});
+    v.push_back({"long -", test_long('-')});
+    v.push_back({"long *", test_long('*')});
+    v.push_back({"long /", test_long('/')});
 
-    results.push_back({"dbl +", test_double('+')});
-    results.push_back({"dbl -", test_double('-')});
-    results.push_back({"dbl *", test_double('*')});
-    results.push_back({"dbl /", test_double('/')});
+    v.push_back({"dbl +", test_double('+')});
+    v.push_back({"dbl -", test_double('-')});
+    v.push_back({"dbl *", test_double('*')});
+    v.push_back({"dbl /", test_double('/')});
 
-    // Пошук максимуму
-    double max_val = 0;
-    for (const auto& r : results) if (r.ops > max_val) max_val = r.ops;
+    double max_v = 0;
+    for(size_t i = 0; i < v.size(); i++) {
+        if(v[i].val > max_v) max_v = v[i].val;
+    }
 
-    // Вивід таблиці
     cout << "----------------------------------------------------------------\n";
     cout << " Type Op     Ops/Sec       Graph\n";
     cout << "----------------------------------------------------------------\n";
     
-    for (const auto& r : results) {
-        int percent = (int)((r.ops / max_val) * 100);
-        int bars = (percent * 20) / 100;
+    for(size_t i = 0; i < v.size(); i++) {
+        int p = (int)((v[i].val / max_v) * 100);
+        int bars = (p * 20) / 100;
         
-        string graph = "[";
-        for(int i=0; i<20; i++) graph += (i < bars ? "=" : " ");
-        graph += "]";
+        string g = "[";
+        for(int j = 0; j < 20; j++) {
+            if (j < bars) g += "=";
+            else g += " ";
+        }
+        g += "]";
 
-        cout << setw(9) << left << r.name 
-             << setw(11) << scientific << setprecision(2) << r.ops 
-             << " " << graph << " " << setw(3) << percent << "%" << endl;
+        cout << setw(9) << left << v[i].name 
+             << setw(11) << scientific << setprecision(2) << v[i].val 
+             << " " << g << " " << setw(3) << right << p << "%" << endl;
     }
     cout << "----------------------------------------------------------------\n";
     
