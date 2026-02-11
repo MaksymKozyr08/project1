56bba6204e45b52144645e7d7fc34e81e9aa0787
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-02-11 22:48:27 +0200

    add

---
+++ Prepare/task7.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <ratio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cstdarg>
+#include <stack>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+ll permutation(vector<ll>& a,ll val){
+    ll k=0;
+    for(ll i=0;i<a.size();++i){
+        if(a[i]!=val){
+            a[k]=a[i];
+            k++;
+        }
+    }
+    return k;
+}
+
+int main(){
+    ll n;
+    cin>>n;
+    vector<ll> a(n);
+    for(ll i=0;i<n;++i){
+        cin>>a[i];
+    }
+    ll val=0;
+    cin>>val;
+    ll k=permutation(a,val);
+    cout<<k;
+    
+    return 0;
+}
