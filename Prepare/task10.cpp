a24f9d115aa4327494b2f7a0c31c914533e849dd
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-02-12 21:08:11 +0200

    add

--- Prepare/task10.cpp
+++ Prepare/task10.cpp
@@ -53,6 +53,17 @@ ll sorting(vector<ll> nums,ll target){
     }
     return left;
 }
+
+ll lastword(string s){
+    ll k=0;
+    for(size_t i=s.length()-1;i>=0;i--){
+        if(s[i]!=' ' && k==0)k++;
+        if(s[i]==' ' && k!=0){
+            k++;
+        }
+    }
+    return k;
+}
 int main(){
     /*
     ll n;
