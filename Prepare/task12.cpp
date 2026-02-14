79d033a69f2e0c014fb56c19a0aa513607c33599
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-02-14 22:56:00 +0200

    add

---
+++ Prepare/task12.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <ratio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cstdarg>
+#include <stack>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+string summary(string a,string b){
+    string result;
+    ll k=0;
+    for(ll i=max(a.size(),b.size());i>=0;i--){
+        if((a[i]+b[i])>1){
+            result[i]=0;
+            k++;
+        }
+        else{
+            result[i]=a[i]+b[i];
+        }
+    }
+    if(k!=0){
+        result.insert(result.begin(),1);
+    }
+    return result;
+}
+int main(){
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    string result=summary(s1,s2);
+    return 0;
+}
