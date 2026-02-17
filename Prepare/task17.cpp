11d0610e379b9b8bc66f92b00681ccf07f449c71
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-02-17 18:22:11 +0200

    add

---
+++ Prepare/task17.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ratio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cstdarg>
+#include <stack>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
