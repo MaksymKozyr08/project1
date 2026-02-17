2f5a356a1a727dbfa3bbd093ee501ef33282fd33
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-02-17 19:00:51 +0200

    finsh 88 problem

--- Prepare/task17.cpp
+++ Prepare/task17.cpp
@@ -18,8 +18,31 @@ typedef long long ll;
 typedef long double ld;
 
 using namespace std;
-
+void result(vector<ll>& nums1,ll n,vector<ll>& nums2,ll m){
+    ll i=n-1;
+    ll j=m-1;
+    ll k=n+m-1;
+    while(j>=0){
+        if(i>=0 && nums2[j]>nums1[i]){
+            nums1[k--]=nums2[j--];
+        }
+        else{
+            nums1[k--]=nums1[i--];
+        }
+    }
+}
 int main(){
-    
+    ll n=0,m=0;
+    cin>>n;
+    cin>>m;
+    vector<ll> a(n+m,0);
+    vector<ll> b(m);
+    for(ll i=0;i<n;++i){
+        cin>>a[i];
+    }
+    for(ll i=0;i<m;++i){
+        cin>>b[i];
+    }
+    result(a,n,b,m);
     return 0;
 }
