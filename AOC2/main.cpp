25c39caaf5b0251a80d622243e3d5c8015303267
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-04-23 12:42:21 +0300

    add

--- AOC2/main.cpp
+++ AOC2/main.cpp
@@ -4,15 +4,21 @@
 
 using namespace std;
 
-uint16_t prog[] = {0x2207, 0x2407,
+uint16_t prog[] = {
+  0x2207, 
+  0x2407,
 
-                   0x94BF, 0x14A1,
+  0x94BF,
+  0x14A1,
 
-                   0x1642,
+  0x1642,
+  0x10E0,
+  0xF027,
+  0xF025,
 
-                   0x10E0, 0xF027, 0xF025,
-
-                   0x0012, 0x0020};
+  0x0000,
+  0x7FFF
+};
 
 int main() {
   FILE *f = fopen("sum.obj", "wb");
