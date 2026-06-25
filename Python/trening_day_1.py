1ed248b08a9fd29f42fcc78eafd706ba06c3880e
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-06-25 20:24:29 +0300

    first problem

---
+++ Python/trening_day_1.py
@@ -0,0 +1,25 @@
+'''
+
+a= int(input("first side: "))
+b=int(input("second side: "))
+print(((a**2)+(b**2))**0.5)
+
+
+c=int(input("input seconds:"))
+a=c//3600
+c=c%3600
+b=c//60
+c=c%60
+print(a," ",b," ",c)
+
+
+a=int(input("input number"))
+print("yep" if 10<=a<=50 else "no")
+
+
+a=int(input())
+print((a % 4 == 0 and a % 100 != 0) or a % 400 == 0)
+
+'''
+
+print("Hello, my name Max, and i am 18 y.o.")
