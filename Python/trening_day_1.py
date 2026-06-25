ac2d718bb780f161907c655735fd8675c1d3e52f
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-06-25 20:45:34 +0300

    add

--- Python/trening_day_1.py
+++ Python/trening_day_1.py
@@ -20,6 +20,26 @@ print("yep" if 10<=a<=50 else "no")
 a=int(input())
 print((a % 4 == 0 and a % 100 != 0) or a % 400 == 0)
 
-'''
 
 print("Hello, my name Max, and i am 18 y.o.")
+
+
+a=str(input("input word for uot the half of word"))
+length=len(a)
+print(a[(length//2-1):(length//2+1)])
+
+
+a = []
+for i in range(3):
+    b = int(input())
+    a.append(b)
+
+a.pop()
+print(a)
+
+a=[]
+a=input().split()
+a[0], a[-1]=a[-1],a[0]
+print(a)
+'''
+
